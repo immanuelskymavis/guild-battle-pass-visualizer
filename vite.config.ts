7a44039c747d3291602a11820f0374d8f1019800
immanuelskymavis <125549990+immanuelskymavis@users.noreply.github.com>
2025-09-26 17:48:42 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
   },
+  base: '/guild-battle-pass-visualizer/',
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      input: {
-        main: './index.html'
-      }
-    }
-  }
+  },
 })
