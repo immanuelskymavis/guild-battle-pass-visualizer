c86d7b0af55fdcc7d6f37cef36aaab817b2a069b
immanuelskymavis <125549990+immanuelskymavis@users.noreply.github.com>
2025-09-26 22:52:57 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/guild-battle-pass-visualizer/' : '/',
+  base: '/guild-battle-pass-visualizer/',
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
-    emptyOutDir: true,
   },
 })
